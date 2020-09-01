e3bad7ac91b197477eabad7bcc1a1489e75bbeb8
Jeremy Schulman <jeremy.schulman@mlb.com>
2020-09-01 11:11:57 -0400

    adding to_csv

--- aioipfabric/client.py
+++ aioipfabric/client.py
@@ -13,6 +13,8 @@
 #  limitations under the License.
 #
 
+from typing import List, Dict, Optional, AnyStr
+import csv
 
 # -----------------------------------------------------------------------------
 # Private Imports
@@ -57,4 +59,46 @@ class IPFabricClient(IPFInventoryMixin):
 
     """
 
-    pass
+    @staticmethod
+    def to_csv(
+        datalist: List[Dict],
+        filepath: AnyStr,
+        fieldnames: Optional[List[AnyStr]] = None,
+        exclude: Optional[List[AnyStr]] = None,
+    ) -> None:
+        """
+        This method will store the given list of dict items to a CSV file.  The
+        CSV column headers will be taken from the first list item keys.  The
+        `exclude` list can be used to omit designated columns from the CSV file,
+        for exampel ['id'] would omit the 'id' column.
+
+        Parameters
+        ----------
+        datalist
+        filepath
+        exclude
+        fieldnames
+        """
+        dl_fieldnames = datalist[0].keys()
+
+        if fieldnames:
+            _s_fieldnames = set(fieldnames)
+            _s_dl_fieldnames = set(dl_fieldnames)
+
+            if _s_fieldnames & _s_dl_fieldnames != _s_fieldnames:
+                raise RuntimeError(f"Invalid set of fieldnames: {fieldnames}")
+
+            exclude = _s_dl_fieldnames - _s_fieldnames
+        else:
+            fieldnames = dl_fieldnames
+
+        if exclude:
+            fieldnames = [col for col in fieldnames if col not in exclude]
+            for rec in datalist:
+                for key in exclude:
+                    del rec[key]
+
+        with open(filepath, "w+") as ofile:
+            csv_wr = csv.DictWriter(ofile, fieldnames=fieldnames)
+            csv_wr.writeheader()
+            csv_wr.writerows(datalist)
